d07c92fb543cae4882be11a93429ed2e1ab7113d
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2022-12-05 21:22:36 +0100

    Bug fix with path

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+  },
+  fontFamily: {
+    Poppins: ['Poppins', 'sans-serif'],
   },
   plugins: [],
 }
