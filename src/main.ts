82cda40fe90960d1bdd6edd36aa8490044ca27d8
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2022-12-07 22:14:39 +0100

    Added time api to get week number

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import './style/style.css';
 
+
+
+
+
 /**
  *  Variables and Selectors
  */
@@ -10,6 +14,7 @@ let month = date.getMonth() + 1;
 let day = date.getDate();
 
 const dayDisplay = document.getElementById("date-stamp");
+const displayWeek = document.getElementById('current-week');
 
 /**
  * Print todays date to date-stamp container. 
@@ -24,3 +29,17 @@ dayDisplay.innerHTML = String(year) + '-' + String(month) + '-' + String(day).pa
 /**
  *  Functions
  */
+
+async function getCurrentTime() {
+    return fetch('http://worldtimeapi.org/api/ip/')
+    .then((data) => data.json())
+    .then((json) => json)
+    .catch((error) => {
+    console.error('Error fetching timezone', error);
+    return null;
+});
+}
+const currentTime = await getCurrentTime();
+displayWeek.innerHTML = '| week: ' + currentTime.week_number
+
+console.log(currentTime);
